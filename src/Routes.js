cac41303679c339695e462fb017d2972c281ba27
Sasiliyu Adetunji <sasil.adetunji@gmail.com>
2019-11-10 16:39:58 +0100

    add crud frontend features

--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,8 @@ import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
 import AppliedRoute from "./components/AppliedRoute";
+import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
 
 export default function Routes({ appProps }) {
   return (
@@ -12,6 +14,8 @@ export default function Routes({ appProps }) {
       <AppliedRoute path="/" exact component={Home} appProps={appProps} />
       <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
       <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
+      <AppliedRoute path="/notes/new" exact component={NewNote} appProps={appProps} />
+      <AppliedRoute path="/notes/:id" exact component={Notes} appProps={appProps} />
       { /* Finally, catch all unmatched routes */ }
       <Route component={NotFound} />
     </Switch>
--- src/config.js
+++ src/config.js
@@ -12,5 +12,6 @@ export default {
       USER_POOL_ID: "us-east-1_MbcWUyERr",
       APP_CLIENT_ID: "4tkpotcu1g74oe07k7kopg1t4k",
       IDENTITY_POOL_ID: "us-east-1:40a530f6-cafd-4df3-bb8e-6b9e69a2f806"
-    }
+    },
+    MAX_ATTACHMENT_SIZE: 5000000,
   };
