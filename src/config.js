c084965210c134c49b53c3d96e24e4bdb8367a23
Sasiliyu Adetunji <sasil.adetunji@gmail.com>
2019-11-10 19:43:16 +0100

    env variable setting

--- src/config.js
+++ src/config.js
@@ -1,4 +1,24 @@
-export default {
+
+const dev = {
+    STRIPE_KEY: "pk_test_5zzkVnCrhr4BRIvcl9WlL5PU00qXklKxvR",
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "sasil-notes-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://vs74jlacy5.execute-api.us-east-1.amazonaws.com/dev"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_MbcWUyERr",
+      APP_CLIENT_ID: "4tkpotcu1g74oe07k7kopg1t4k",
+      IDENTITY_POOL_ID: "us-east-1:40a530f6-cafd-4df3-bb8e-6b9e69a2f806"
+    }
+  };
+
+const prod = {
+    STRIPE_KEY: "pk_test_5zzkVnCrhr4BRIvcl9WlL5PU00qXklKxvR",
     s3: {
       REGION: "us-east-1",
       BUCKET: "sasil-notes-uploads"
@@ -12,7 +32,16 @@ export default {
       USER_POOL_ID: "us-east-1_MbcWUyERr",
       APP_CLIENT_ID: "4tkpotcu1g74oe07k7kopg1t4k",
       IDENTITY_POOL_ID: "us-east-1:40a530f6-cafd-4df3-bb8e-6b9e69a2f806"
-    },
+    }
+  };
+
+  // Default to dev if not set
+  const config = process.env.REACT_APP_STAGE === 'prod'
+    ? prod
+    : dev;
+
+  export default {
+    // Add common config values here
     MAX_ATTACHMENT_SIZE: 5000000,
-    STRIPE_KEY: "pk_test_5zzkVnCrhr4BRIvcl9WlL5PU00qXklKxvR",
+    ...config
   };
